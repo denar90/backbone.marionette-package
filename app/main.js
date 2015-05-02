4265393eeca0db3ceb6c4f0381921d997d21c883
Artem Denysov <denysov.artem@gmail.com>
2015-05-02 16:58:11 +0300

    change app folder name

--- app/main.js
+++ app/main.js
@@ -1,11 +1,11 @@
 require.config({
 	paths : {
-		underscore : '../bower_components/underscore/underscore',
-		backbone   : '../bower_components/backbone/backbone',
-		marionette : '../bower_components/marionette/lib/backbone.marionette',
-		jquery     : '../bower_components/jquery/dist/jquery',
-		handlebars : '../bower_components/handlebars/handlebars',
-		text       : '../bower_components/text/text'
+		underscore : '../vendor/underscore/underscore',
+		backbone   : '../vendor/backbone/backbone',
+		marionette : '../vendor/marionette/lib/backbone.marionette',
+		jquery     : '../vendor/jquery/dist/jquery',
+		handlebars : '../vendor/handlebars/handlebars',
+		text       : '../vendor/requirejs-text/text'
 	},
 	shim : {
 		underscore: {
